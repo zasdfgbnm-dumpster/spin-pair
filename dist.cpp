34e7b8f32952a233c5fa4cab89011183b4fc994d
Gao Xiang <qasdfgtyuiop@gmail.com>
2013-10-04 10:00:08 +0800

    add dist

---
+++ dist.cpp
@@ -0,0 +1,11 @@
+#include <limits>
+#include "pair.hpp"
+
+double count[200][200] = { 0.0 };
+
+int main(){
+	double max =  numeric_limits<double>::infinity();
+	double min = -numeric_limits<double>::infinity();
+	while(true){
+	}
+}
