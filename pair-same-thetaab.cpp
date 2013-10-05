7781e58eb88b3a42cfbeda45457fab7b751e1f11
Gao Xiang <qasdfgtyuiop@gmail.com>
2013-10-05 12:02:11 +0800

    calculate until n=9 for pes

--- pair-same-thetaab.cpp
+++ pair-same-thetaab.cpp
@@ -1,17 +1,18 @@
 #include "pair.hpp" 
 
 int main(){
-	for(int n=1;n<=4;n++) {
+	#pragma omp parallel for
+	for(int n=1;n<=8;n++) {
 		auto pairs = random_pairs(n);
-		double cor = pairs[0].omegar();
-		double ratios[] = { 0.9,0.95,0.98,1,1.02,1.05,1.1 };
-		for(auto &ratio:ratios) {
-			double w1 = cor*ratio;
-			stringstream fnstream;
-			fnstream << "n=" << n << ",w1=" << w1/1_MHz << "MHz.txt";
-			string suffix = fnstream.str();
+		double w1 = pairs[0].omegar();
+		stringstream fnstream;
+		fnstream << "n=" << n << ",w1=" << w1/1_MHz << "MHz.txt";
+		string suffix = fnstream.str();
+		if(n<=4) {
 			ofstream pairs_out(string("pairs-")+suffix);
 			pairs_simulation(w1,pairs,pairs_out);
 		}
+		ofstream pes_out(string("pes-")+suffix);
+		pes_simulation(w1,pairs,pes_out);
 	}
 }
